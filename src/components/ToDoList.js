97e396ecb57f43d175dc0ac1539b9d06a94b2d6d
mrbks1102 <[mrbks1102@gmail.com]>
2020-12-03 18:27:24 +0900

    [add]ToDoのタイトルを表示するコンポーネント作成

---
+++ src/components/ToDoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../css/ToDoList.css';
+
+//状態を持たない、propsに渡された引数によって表示内容が変わるだけのコンポーネント
+class ToDoList extends Component {
+  render() {
+    //todoリストのタイトルを定義
+    const {
+      title
+    } = this.props;
+
+    return (
+      <div className="ToDoList">
+        <div className="ToDoList_title">{title}</div>
+      </div>
+    )
+  }
+}
+
+export default ToDoList;
