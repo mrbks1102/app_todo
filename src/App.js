3ed3e2a57871944471cc9e3699e9420b6f3995e8
mrbks1102 <[mrbks1102@gmail.com]>
2020-12-03 23:40:28 +0900

    [add]ToDo追加機能実装

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import './App.css';
 import ToDoList from "./components/ToDoList.js"
 
 class App extends Component {
+  state = {
+    todoList: []
+  }
+
   render() {
     return (
       <div className="App">
         <form
-          className="App_form">
+          className="App_form"
+          onSubmit={e => {
+            // フォームが送信されてしまうので無効化し更新されないようにする
+            e.preventDefault();
+
+            // idがtitleのvalueを取得
+            const title = e.target.title.value;
+
+            // todoList stateに追加
+            this.setState(
+              {
+                todoList: this.state.todoList.concat({
+                  title: title
+                })
+              },
+              // stateの変更後に入力した値を空にする(フォームの表示)
+              () => {
+                //constで定義した物は再代入できない為再度記述
+                e.target.title.value = "";
+              }
+            )
+          }}
+        >
           <div>
             <input
               type="text"
@@ -17,12 +43,18 @@ class App extends Component {
           </div>
           <div>
             <button type="submit">
-              投稿
+              登録
             </button>
           </div>
         </form>
         <div>
-          <ToDoList title="ToDoリスト" />
+          {/* todoList配列の要素数分ToDoListコンポーネントを表示 */}
+          {this.state.todoList.map(todo => (
+            <ToDoList
+              key={todo.title}
+              title={todo.title}
+            />
+          ))}
         </div>
       </div>
     );
